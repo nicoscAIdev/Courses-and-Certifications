a42fd5443c2335af2a9492a20ab530c23727f006
nicoscAIdev <nicoscaidev@gmail.com>
2025-03-12 12:37:00 -0300

    Changes added

--- Test.py
+++ Test.py
@@ -1,3 +1,9 @@
+#Ejercicio de porcentajes
+
+#Juan tiene 3 manzanas y 1 pera Pedro tiene 2 manzanas y 4 peras
+
+#¿Que porcentaje de frutas tienen en total?
+
 import pandas as pd
 
 df = pd.DataFrame({"manzanas": [3, 2], "peras": [1, 4]}, index=["Juan", "Pedro"])
