ba2f2e46c86ce2a79cf4f79b27ea505be7e1536d
nicoscAIdev <nicoscaidev@gmail.com>
2025-03-30 20:35:15 -0300

    Changes

--- Diploma in Artificial Intelligence/1- Introduction to Python/Modules/Algorithms and Functions.py
+++ Diploma in Artificial Intelligence/1- Introduction to Python/Modules/Algorithms and Functions.py
@@ -78,6 +78,28 @@ fibo()
 para guardar el elemento mas chico de una lista
 en otra."""
 
+def merge_sorted(lista_1,lista_2):
+    resultado = []
+    while len(lista_1) > 0 or len(lista_2) > 0: # mientras alguna lista tenga valores
+        # Vamos a recorrer las listas hasta que no queden más elementos en ninguna.
+        if len(lista_1) > 0: # si la primera lista tiene valores
+            if len(lista_2) > 0:
+                # Este es el caso donde quedan elementos en las dos listas.
+                # Aquí elegimos el menor de los primeros elementos
+                if lista_1[0] <= lista_2[0]:
+                    resultado.append(lista_1.pop(0))
+                else:
+                    resultado.append(lista_2.pop(0))
+            else:
+                # Este es el caso donde lista_1 todavía tiene elementos pero lista_2 no
+                resultado.append(lista_1.pop(0))
+        else:
+            # Si no quedan elementos en la lista_1 pero seguimos dentro del while loop,
+            # es porque quedan elementos en la lista2
+            resultado.append(lista_2.pop(0))
+    return resultado
+
+
 # Algoritmo 4
 
 def bubble_sort(lista):
