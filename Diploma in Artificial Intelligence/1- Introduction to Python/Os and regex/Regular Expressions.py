7ad5630046a5970404a43643b0fd9fff4ea59ecc
nicoscAIdev <nicoscaidev@gmail.com>
2025-05-03 23:04:30 -0300

    cosas

--- Diploma in Artificial Intelligence/1- Introduction to Python/Os and regex/Regular Expressions.py
+++ Diploma in Artificial Intelligence/1- Introduction to Python/Os and regex/Regular Expressions.py
@@ -6,3 +6,14 @@ regla_de_busqueda = "\d+"
 # print(texto)
 print(re.findall(regla_de_busqueda, texto))
 
+texto = "Mi nombre es Carlos y mi teléfono es 114232324 154232324"
+regla_de_busqueda = "(?:15|11)\d+"
+re.findall(regla_de_busqueda,texto)
+
+texto = "Mi nombre es asfasfeaf33 y mi teléfono es 11 6423-2324"
+regla_de_busqueda = "(?:15|11)[0-9\s-]{6,10}"
+re.findall(regla_de_busqueda,texto)
+
+texto = "REPORTE DE PERFOMANCE - MES DE julio y agosto"
+regla_de_busqueda = "(MES DE (?:JULIO|AGOSTO|JUNIO))"
+re.findall(regla_de_busqueda,texto, flags=re.IGNORECASE)
