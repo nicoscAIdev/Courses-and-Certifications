c058b02fa73cf958b5744913505896e1030eccfb
nicoscAIdev <nicoscaidev@gmail.com>
2025-05-25 22:26:39 -0300

    Exercise completed

--- Diploma in Artificial Intelligence/1- Introduction to Python/6. Intro a POO/Exercise 4.py
+++ Diploma in Artificial Intelligence/1- Introduction to Python/6. Intro a POO/Exercise 4.py
@@ -19,3 +19,34 @@
 # 3. Ejemplo de uso:
 #    - Crear una instancia de Circulo y mostrar su área
 #    - Crear una instancia de Cilindro y mostrar su volumen
+
+import math
+
+class Circulo:
+
+    def __init__(self, radio):
+        self.radio = radio
+
+    def area(self):
+        return math.pi * self.radio ** 2
+    
+    def get_radio(self):
+        return self.radio
+
+class Cilindro(Circulo):
+    
+    def __init__(self, radio, altura):
+        super().__init__(radio)
+        self.altura = altura
+
+    def volumen(self):
+        return self.area() * self.altura
+    
+if __name__ == "__main__":
+    
+    mi_circulo = Circulo(4.12)
+    print(f"Área del círculo: {mi_circulo.area():.2f}")
+    print(f"Radio del círculo: {mi_circulo.get_radio()}")
+
+    mi_cilindro = Cilindro(4.12, 10)
+    print(f"Volumen del cilindro: {mi_cilindro.volumen():.2f}")
